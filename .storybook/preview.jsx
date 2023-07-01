5bbe8881ffaa5a62f91169e3e1e071a873251900
Francois Verin <francois@tekyn.com>
2023-07-01 17:11:08 +0200

    💄: integrate emotion with storybook, provide theme to storybook, create Button stories

---
+++ .storybook/preview.jsx
@@ -0,0 +1,29 @@
+import { Global, ThemeProvider } from '@emotion/react';
+import theme from '../src/theme';
+import { withThemeFromJSXProvider } from '@storybook/addon-styling';
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  
+};
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    themes: {
+      default: theme,
+    },
+    defaultTheme: 'default',
+    Provider: ThemeProvider,
+  }),
+]
+
+export default preview;
