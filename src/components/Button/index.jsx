db4e53cb81ed13ec274176620e40c3848b8f3dc9
Francois Verin <francois@tekyn.com>
2023-07-03 00:24:46 +0200

    ✨ (Button): add onClick props

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,9 @@
 import { ButtonContainer } from "./styled";
 import PropTypes from 'prop-types'
+import { noop } from "../../utils/methods/noop";
 
-const Button = ({className, variantType, label}) => {
-  return <ButtonContainer className={className} variantType={variantType}>{label}</ButtonContainer>;
+const Button = ({className, variantType, label, onClick}) => {
+  return <ButtonContainer className={className} variantType={variantType} onClick={onClick}>{label}</ButtonContainer>;
 };
 
 Button.propTypes = {
@@ -18,12 +19,17 @@ Button.propTypes = {
    * Optional variantType
    */
   variantType: PropTypes.oneOf(['fill', 'stroke']),
+  /**
+   * Optional onClick
+   */
+  onClick: PropTypes.func
 }
 
 Button.defaultProps = {
   className: null,
   label: null,
   variantType: 'fill',
+  onClick: noop,
 }
 
 export default Button;
