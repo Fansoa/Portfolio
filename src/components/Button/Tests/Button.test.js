e522ef04084f816317938d09f3725f3363be0f68
Francois Verin <francois@tekyn.com>
2023-07-03 00:27:53 +0200

    ✅ (Button): update test

--- src/components/Button/Tests/Button.test.js
+++ src/components/Button/Tests/Button.test.js
@@ -1,14 +1,20 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Button from "..";
 import IntegrationProvider from "../../../../configs/jest/testUtils";
+import { noop } from "../../../utils/methods/noop";
+
+const mockOnClick = jest.fn(noop);
 
 test("test Button", async () => {
-  const result = render(
+  const { container } = render(
     <IntegrationProvider>
-      <Button label="Label" />
+      <Button label="Label" onClick={mockOnClick} />
     </IntegrationProvider>
   );
-  expect(result).toMatchSnapshot();
-  screen.getByText("Label");
+  const component = screen.getByText(/label/i);
+  fireEvent.click(component);
+  expect(container).toMatchSnapshot();
+  expect(mockOnClick).toBeCalledTimes(1);
+  screen.getByText(/label/i);
 });
