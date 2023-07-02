a314564f5548e204f025ef9a99757033c4d44922
Francois Verin <francois@tekyn.com>
2023-07-03 00:25:31 +0200

    💄 (Button): update story

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,6 +3,22 @@ import Button from ".";
 export default {
   title: "components/Button",
   component: Button,
+  tags: ["autodocs"],
+  args: {
+    label: "Bouton",
+    variantType: "fill",
+  },
+  argTypes: {
+    className: {
+      table: {
+        disable: true,
+      },
+    },
+    onClick: {
+      action: "Button is clicked",
+      description: "description",
+    },
+  },
 };
 
 export const Default = {};
