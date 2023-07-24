a088978ff0f478b2432e9270caa1d459781ef9b9
Francois Verin <francois@tekyn.com>
2023-07-24 11:17:28 +0200

    💄(global): add colors

--- src/theme/global/index.js
+++ src/theme/global/index.js
@@ -1,7 +1,20 @@
 export const global = {
   colors: {
-    red: "red",
-    green: "green",
     blue: "blue",
+
+    oldBlurple: "#7289DA",
+    fullWhite: "#FFFFFF",
+    greyple: "#99AAB5",
+    darkButNotBlack: "#2C2F33",
+    notQuiteBlack: "#23272A",
+    actuallyBlack: "#000000",
+
+    blurple: "#5865F2",
+    green: "#57F2B7",
+    yellow: "#FEE25C",
+    fuchsia: "#5865F2",
+    red: "#ED4245",
+
+    webdriverIoOrange: "#EA5906",
   },
 };
