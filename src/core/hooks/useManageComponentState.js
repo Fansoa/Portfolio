24e3faa43336df907b1bb6e6c13b7c8578602610
Francois Verin <francois@tekyn.com>
2023-08-13 19:13:29 +0200

    ✨(core): add custom hook to manage componentState

---
+++ src/core/hooks/useManageComponentState.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const useManageComponentState = (inputFieldValue) => {
+  const [isHover, setIsHover] = useState(false);
+  const [isFocus, setIsFocus] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const [isFilled, setIsFilled] = useState(
+    !!inputFieldValue || inputFieldValue === 0,
+  );
+
+  const handleOnBlur = (e) => {
+    const hasValue = Boolean(e.target.value);
+    setIsFilled(hasValue);
+  };
+
+  return {
+    isHover,
+    setIsHover,
+    isFocus,
+    setIsFocus,
+    isActive,
+    setIsActive,
+    isFilled,
+    setIsFilled,
+    handleOnBlur,
+  };
+};
+
+export default useManageComponentState;
