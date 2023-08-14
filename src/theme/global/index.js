4f6ed8c22468bdc5cc459f182757446592fac4fe
Francois Verin <francois@tekyn.com>
2023-08-14 10:02:57 +0200

    ✨: add pokedex colors to theme

--- src/theme/global/index.js
+++ src/theme/global/index.js
@@ -17,4 +17,35 @@ export const global = {
 
     webdriverIoOrange: "#EA5906",
   },
+  pokedex: {
+    colors: {
+      mostlyDesaturatedDarkYellow: "#A8A87B", // normal
+      brightOrange: "#EE803B", // fire
+      softBlue: "#6A92ED", // water
+      moderateGreen: "#7BC757", // grass
+      brightYellow: "#F7CF43", // electric
+      slightlyDesaturatedCyan: "#9AD8D8", // ice
+      strongRed: "#BE322E", // fight
+      darkModerateMagenta: "#9B4A99", // poison
+      softOrange: "#DFBF6E", // ground
+      verySoftBlue: "#A893ED", // flying
+      softPink: "#EC5B89", // psychic
+      moderateYellow2: "#A8B732", // bug
+      moderateYellow: "#B89F41", // rock
+      mostlyDesaturatedDarkViolet: "#705A97", // ghost
+      veryDarkGrayishOrange: "#705849", // dark
+      brightViolet: "#7043F4", // dragon
+      grayishBlue: "#B8B9CF", // steel
+      verySoftRed: "#EFB7BD", // fairy
+      moderateBlue: "#38A8D5", // caseFilled
+      softOrange2: "#F4A261", // buttonColor
+      veryDarkDesaturatedBlue: "#264653", // fontColor
+      softRed: "#E76F51", // fontColor description
+      lightGray: "#C4C4C4", // backgroundColor stats
+      veryLightGray: "#DEDEDE", // borderColor caseStats
+      darkGray: "#8f8f8fde", // fontColor form 87% opac
+      white: "#FFFFFF",
+      whiteSmoke: "#F5F5F5", // backgroundColor TextField
+    },
+  },
 };
