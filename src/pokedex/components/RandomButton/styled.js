77819b41ea54db58ec2a43f1b43179bc70db88da
Francois Verin <francois@tekyn.com>
2023-08-15 14:11:58 +0200

    💄(RandomButton): correct style to get same design on browser & storybook

--- src/pokedex/components/RandomButton/styled.js
+++ src/pokedex/components/RandomButton/styled.js
@@ -1,13 +1,14 @@
 import styled from "@emotion/styled";
 
-export const SearchButtonContainer = styled.div`
+export const RandomButtonContainer = styled.div`
   ${({ theme }) => `
+    box-sizing: border-box;
     border-radius: 80px;
     padding: 20px 18px 20px 18px;
     background: ${theme.pokedex.colors.softOrange2};
-    width: fit-content;
     color: ${theme.pokedex.colors.white};
+    height: 65px;
     display: flex;
-    justify-content: space-around;
+    align-items: center;
   `}
 `;
