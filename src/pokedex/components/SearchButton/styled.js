5b3393004cd5054c736e3a7ce242ae482c2ae64c
Francois Verin <francois@tekyn.com>
2023-08-15 14:12:15 +0200

    💄(SearchButton): correct style to get same design on browser & storybook

--- src/pokedex/components/SearchButton/styled.js
+++ src/pokedex/components/SearchButton/styled.js
@@ -2,13 +2,17 @@ import styled from "@emotion/styled";
 
 export const SearchButtonContainer = styled.div`
   ${({ theme }) => `
+    box-sizing: border-box;
     border-radius: 80px;
     padding: 24px 45px 24px 45px;
     background: ${theme.pokedex.colors.softOrange2};
+    height: 65px;
     width: fit-content;
     font-family: Roboto;
     font-size: 16px;
     font-weight: 700;
     color: ${theme.pokedex.colors.white};
+    display: flex;
+    align-items: center;
   `}
 `;
