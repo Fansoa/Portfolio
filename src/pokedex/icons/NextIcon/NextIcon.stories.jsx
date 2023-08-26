ced0186b11af191a9e5a0492a2f4b030254948cd
Francois Verin <francois@tekyn.com>
2023-08-26 23:39:10 +0200

    ✨(NextIcon): add NextIcon

---
+++ src/pokedex/icons/NextIcon/NextIcon.stories.jsx
@@ -0,0 +1,11 @@
+import NextIcon from ".";
+
+export default {
+  title: "Pokedex/Icons/NextIcon",
+  component: NextIcon,
+  tags: ["autodocs"],
+};
+
+export const NextIconStory = {
+  name: "NextIcon",
+};
