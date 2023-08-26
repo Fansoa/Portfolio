9797ad6b15597e110292c1bf2424a82ecdeab0a7
Francois Verin <francois@tekyn.com>
2023-08-26 23:35:14 +0200

    ✨(VectorIcon): add VectorIcon

---
+++ src/pokedex/icons/VectorIcon/VectorIcon.stories.jsx
@@ -0,0 +1,11 @@
+import VectorIcon from ".";
+
+export default {
+  title: "Pokedex/Icons/VectorIcon",
+  component: VectorIcon,
+  tags: ["autodocs"],
+};
+
+export const VectorIconStory = {
+  name: "VectorIcon",
+};
